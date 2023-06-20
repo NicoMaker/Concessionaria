fed64748e0de5a8a86140c4cfa577421b8b39e99
Nicola.Marano <nicola.marano@stud.itsaltoadriatico.it>
2023-06-20 16:54:11 +0200

    Add project files.

---
+++ Concessionaria/Program.cs
@@ -0,0 +1,34 @@
+﻿using Concessionaria;
+
+Veicoli veicolo = new Veicoli();
+
+veicolo.model.Add("Porshe Caarrera");
+veicolo.typology.Add("maccchina");
+veicolo.displacement.Add(3000);
+veicolo.colour.Add("Bianco");
+veicolo.port.Add(3);
+veicolo.seats.Add(2);
+
+veicolo.model.Add("BMW GT2");
+veicolo.typology.Add("moto");
+veicolo.displacement.Add(1250);
+veicolo.colour.Add("Nera");
+veicolo.port.Add(0);
+veicolo.seats.Add(2);
+
+veicolo.model.Add("Ferrari SF90 Spider");
+veicolo.typology.Add("macchina");
+veicolo.displacement.Add(8000);
+veicolo.colour.Add("Rossa");
+veicolo.port.Add(3);
+veicolo.seats.Add(2);
+
+veicolo.model.Add("Yamaha Xmax 300");
+veicolo.typology.Add("moto");
+veicolo.displacement.Add(1300);
+veicolo.colour.Add("Nera");
+veicolo.port.Add(0);
+veicolo.seats.Add(2);
+
+
+veicolo.visualizza(veicolo.model, veicolo.typology, veicolo.displacement, veicolo.colour, veicolo.port, veicolo.seats);
---
+++ Concessionaria/Veicoli.cs
@@ -0,0 +1,25 @@
+﻿namespace Concessionaria;
+
+internal class Veicoli
+{
+    public List<string> model = new List<string>();
+    public List<string> typology = new List<string>();
+    public List<int> displacement = new List<int>();
+    public List<string> colour = new List<string>();
+    public List<int> port = new List<int>();
+    public List<int> seats = new List<int>();
+
+    public void visualizza(List<string> model, List<string> typology, List<int> displacement, List<string> colour, List<int> port, List<int> seats)
+    {
+        for (int i = 0; i < model.Count; i++)
+        {
+            Console.WriteLine($"il {i + 1} modello ha le seguenti caratteristiche");
+            Console.WriteLine("modello " + model[i]);
+            Console.WriteLine("tipologia " + typology[i]);
+            Console.WriteLine("cilindrata " + displacement[i]);
+            Console.WriteLine("colore " + colour[i]);
+            Console.WriteLine("numero porte " + port[i]);
+            Console.WriteLine("posti a sedere " + seats[i]);
+        }
+    }
+}
